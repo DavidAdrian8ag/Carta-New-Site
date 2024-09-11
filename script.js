d084d604dfc05520f4b4241e47b5fd9ee9b6152a
David Adrián Ochoa <146686328+DavidAdrian8ag@users.noreply.github.com>
2024-09-11 10:59:33 -0600

    Corazon Click

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.title').click(function () {
+        $('.container').addClass('open');
+    });
+    $('.close').click(function () {
+        $('.container').removeClass('open');
+    });
+});
